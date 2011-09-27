6b65729512c56d47ac555ec5c2fedc3b164627d0
Tim Romberg <tim.jakobsson@gmail.com>
2011-09-27 20:36:25 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Starflick::Application.config.secret_token = '424babb3217e3e9a6d6a166d9a32616af9623b2bc5c8f4db97f08e8c20dea460b32b9e216081a3b681da42202640bb9296a92458b02732fd5ab9295cc02cf76a'
